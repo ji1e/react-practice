2a9fc3169024440e81d81b781dfec07b19b27e9b
ji1e <code3784@gmail.com>
2023-07-10 12:50:40 +0900

    feat: 좋아요 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [like, setLike] = useState(0);
+  const [todoLists, setTodoLists] = useState([
+    { id: nanoid(), content: "자바스크립트 공부하기", likes: 0 },
+    { id: nanoid(), content: "리액트 공부하기", likes: 0 },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        value={newTodo}
+        onChange={(e) => {
+          setNewTodo(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          const todoCopy = [...todoLists, { id: nanoid(), content: newTodo, likes: 0 }];
+          setTodoLists(todoCopy);
+          setNewTodo("");
+        }}
+      >
+        입력값 추가
+      </button>
+      {/* map의 인자 : map(item, index) */}
+      {todoLists.map((todos) => {
+        return (
+          <div className="todolists" key={todos.id}>
+            <button
+              // 삭제하기
+              onClick={() => {
+                const filterTodos = todoLists.filter((todo) => {
+                  return todo.id !== todos.id;
+                });
+                setTodoLists(filterTodos);
+              }}
+            >
+              삭제
+            </button>
+            <span> {todos.content} </span>
+            {/* 좋아요 기능 */}
+            <div className="like">
+              <button
+                onClick={() => {
+                  const updatedTodoLists = todoLists.map((todo) => {
+                    if (todo.id === todos.id) {
+                      return { ...todo, likes: todo.likes + 1 };
+                    }
+                    return todo;
+                  });
+                  setTodoLists(updatedTodoLists);
+                }}
+              >
+                ♥︎ {todos.likes}
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
